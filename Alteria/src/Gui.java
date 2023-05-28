cbd33c9decdfde21a94f1278d4a727bb3e710c45
jose victor garcia llorente <victorgarciallorente20@gmail.com>
2023-05-28 22:23:35 +0200

    Gui Class saveData and loadData commented + spaces

--- Alteria/src/Gui.java
+++ Alteria/src/Gui.java
@@ -1,10 +1,10 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+//import java.io.FileInputStream;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.ObjectInputStream;
+//import java.io.ObjectOutputStream;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -15,49 +15,49 @@ public class Gui implements ActionListener{
     }
 
     public void main(){
-        JFrame frame = new JFrame();
-        frame.setVisible(true);
+    JFrame frame = new JFrame();
+    frame.setVisible(true);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setLocationRelativeTo(null);
 
-        JButton button1 = new JButton("Encrypt");
-        JButton button2 = new JButton("Decrypt"); 
-        JButton buttonAux = new JButton("");
-        frame.add(button1);
-        frame.add(button2);
-        frame.add(buttonAux);
-        frame.setSize(400,200);
-        button1.setBounds(0 , 50 , 200 , 100);
-        button2.setBounds(200 , 50 , 200 , 100);
+    JButton button1 = new JButton("Encrypt");
+    JButton button2 = new JButton("Decrypt"); 
+    JButton buttonAux = new JButton("");
+    frame.add(button1);
+    frame.add(button2);
+    frame.add(buttonAux);
+    frame.setSize(400,200);
+    button1.setBounds(0 , 50 , 200 , 100);
+    button2.setBounds(200 , 50 , 200 , 100);
 
-        FolderIterator fi = new FolderIterator("/Users/josevictorgarciallorente/Desktop/test");
+    FolderIterator fi = new FolderIterator("/Users/josevictorgarciallorente/Desktop/test");
 
-        button1.addActionListener(new Gui() {
-            public void actionPerformed(ActionEvent e){
-                if (!fi.isEncrypted()){
-                    fi.encryptDirectory();
-                }
+    button1.addActionListener(new Gui() {
+        public void actionPerformed(ActionEvent e){
+            if (!fi.isEncrypted()){
+                fi.encryptDirectory();
             }
-        });
-
-        button2.addActionListener(new Gui() {
-            public void actionPerformed(ActionEvent e){
-                fi.decryptDirectory();
-                fi.reset();
-            }
-        });
-
         }
+    });
 
-        @Override                                   //Este metodo es inutil, pero lo pongo sin implementar para que no de error
-        public void actionPerformed(ActionEvent e) {
-            throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    button2.addActionListener(new Gui() {
+        public void actionPerformed(ActionEvent e){
+            fi.decryptDirectory();
+            fi.reset();
         }
+    });
+
+    }
+
+    @Override                                   //Este metodo es inutil, pero lo pongo sin implementar para que no de error
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
 
 
-    public void saveData(){
-        String file = /* Gui.CONFIGURATIONFOLDER + File.separatorChar + */ "ConfigFiles";
+    /*public void saveData(){
+        String file =  Gui.CONFIGURATIONFOLDER + File.separatorChar + "ConfigFiles";
         try{
             FileOutputStream fos = new FileOutputStream(file);
             ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -73,7 +73,7 @@ public class Gui implements ActionListener{
 
     public Gui loadData(){
         Gui gui = new Gui();
-        String file = /* Gui.CONFIGURATIONFOLDER + File.separatorChar + */ "ConfigFiles";
+        String file = Gui.CONFIGURATIONFOLDER + File.separatorChar + "ConfigFiles";
         try {
             FileInputStream fis = new FileInputStream(file);
             ObjectInputStream ois = new ObjectInputStream(fis);
@@ -86,5 +86,5 @@ public class Gui implements ActionListener{
             System.out.println("Error loading configuration files");
         }
         return gui;
-    }
+    }*/
 }
